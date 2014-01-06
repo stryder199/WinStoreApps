2acfcb6ffa45afaca9ef5089c1f121f60a0cd09e
Mark Lessard <lessardme@hotmail.com>
2014-01-06 15:51:56 -0800

    init

---
+++ FileExplorer/FileExplorer/MainPage.xaml.cpp
@@ -0,0 +1,60 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace FileExplorer;
+
+using namespace Platform;
+using namespace Windows::Storage;
+using namespace Windows::Storage::BulkAccess;
+using namespace Windows::Storage::FileProperties;
+using namespace Windows::Storage::Search;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	StorageFolder^ picturesFolder = KnownFolders::PicturesLibrary;
+	
+	QueryOptions^ queryOptions = ref new QueryOptions();
+	queryOptions->FolderDepth = FolderDepth::Deep;
+	queryOptions->IndexerOption = IndexerOption::UseIndexerWhenAvailable;
+
+	StorageItemQueryResult^ folderQueryResult = picturesFolder->CreateItemQueryWithOptions(queryOptions);
+
+	FileInformationFactory^ ff = ref new FileInformationFactory(folderQueryResult, ThumbnailMode::ListView);
+	
+	IAsyncOperation<IVectorView<IStorageItemInformation^>^>^ filesystemQueryAsync = ff->GetItemsAsync();
+
+	IVectorView<IStorageItemInformation^>^ fsItemVec = filesystemQueryAsync->GetResults();
+
+
+
+}
+
+
+void FileExplorer::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+}
+
+
+void FileExplorer::MainPage::Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+}
---
+++ FileExplorer/FileExplorer/MainPage.xaml.h
@@ -0,0 +1,47 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace FileExplorer
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+
+	/*public ref class FileSystemItem
+	{
+	public:
+
+	private:
+		std::string name, path;
+	};
+
+	public ref class File : public FileSystemItem
+	{
+	public:
+
+	private:
+		std::string thumbnail;
+	};
+
+	public ref class Folder : public FileSystemItem
+	{
+	public:
+
+	private:
+	};*/
+}
